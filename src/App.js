cb8a8373a8433ca2c026e56db2456a8843719bd5
Abdul Bashiru <abdulbashiru598@gmail.com>
2025-08-02 09:23:01 +0300

    Maelezo ya mabadiliko

---
+++ src/App.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 50 }}>
+      <h1>Welcome to VibeTZ Web!</h1>
+      <p>Your social media app is live.</p>
+    </div>
+  );
+}
+
+export default App;
