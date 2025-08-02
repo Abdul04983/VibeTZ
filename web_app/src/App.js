62b3dca3ab69ee0fad3ad38a5d59e5e31a8c45ad
Abdul Bashiru <abdulbashiru598@gmail.com>
2025-08-02 10:08:39 +0300

    Fix: Created src/App.js with default content

---
+++ web_app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#5F27CD' }}>Karibu kwenye VibeTZ Web ??</h1>
+      <p>App yako iko hewani kikamilifu kupitia Vercel ??</p>
+      <p>Endelea kuijenga hapa ndani ya <code>web_app/src</code>.</p>
+    </div>
+  );
+}
+
+export default App;
